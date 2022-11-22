a4c1b9ec962e026e5380cd8baf5243f183cb4599
sundeepchennupati <114455151+sundeepchennupati@users.noreply.github.com>
2022-11-22 16:40:39 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "AWSEC2Instance"{
     instance_type = "t2.micro"
     key_name = "sunproddevops"
     security_groups = ["launch-wizard-11"]
-    tags{
+    tags = {
         name = "tomcatserver"
     }
 }
