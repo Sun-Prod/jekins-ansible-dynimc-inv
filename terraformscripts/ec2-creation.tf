8ec02b7c63114b14e44ff6614e012ecca9ed8beb
sundeepchennupati <114455151+sundeepchennupati@users.noreply.github.com>
2022-11-22 16:42:30 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 resource "aws_instance" "AWSEC2Instance"{
     count = 1
-    ami = "ami-050c88925ff2bb50"
+    ami = "ami-050c88925ffb2bb50"
     instance_type = "t2.micro"
     key_name = "sunproddevops"
     security_groups = ["launch-wizard-11"]
